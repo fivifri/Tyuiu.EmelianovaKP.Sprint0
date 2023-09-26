535bc16ef16bc7b8f2331a70a4aaced2010bfce0
fivifri <emelianova.kp@phystech.edu>
2023-09-26 23:25:58 +0500

    Task2

---
+++ Tyuiu.EmelianovaKP.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.EmelianovaKP.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.EmelianovaKP.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Ksenya";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Hello, Ksenya", res);
+        }
+    }
+}
