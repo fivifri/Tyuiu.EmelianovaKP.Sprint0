17d234fa3027a298d2265186cc238f753313c204
fivifri <emelianova.kp@phystech.edu>
2023-09-26 21:19:14 +0500

    Task1

---
+++ Tyuiu.EmelianovaKP.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.EmelianovaKP.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+
+            string name = "Ksenya";
+            int age = 19;
+
+            Console.WriteLine("Hello, my name is " + name + ". I'm " + age + " years old.");
+
+            Console.ReadKey();
+        }
+    }
+}
