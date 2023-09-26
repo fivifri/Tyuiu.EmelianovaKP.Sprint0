c02cb7b7d1b98ec3ae6b9eb593660a67fc8f5e4d
fivifri <emelianova.kp@phystech.edu>
2023-09-27 00:05:42 +0500

    Task4

---
+++ Tyuiu.EmelianovaKP.Sprint0.Task4.V0,Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.EmelianovaKP.Sprint0.Task4.V0,Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.EmelianovaKP.Sprint0.Task4.V0,Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec8b4341-14d7-42c4-9ca3-13e20d64b537")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
