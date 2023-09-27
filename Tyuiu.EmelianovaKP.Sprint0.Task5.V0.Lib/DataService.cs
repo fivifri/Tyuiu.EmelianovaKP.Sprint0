7a87997b19e93de759ae83849299ee4b790b6e5a
fivifri <emelianova.kp@phystech.edu>
2023-09-28 00:13:08 +0500

    Task5

---
+++ Tyuiu.EmelianovaKP.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.EmelianovaKP.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("The variable b = {0}. You can't divide by 0.", b);
+                return -1;
+            }
+
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.EmelianovaKP.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.EmelianovaKP.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.EmelianovaKP.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Addition(10, 5));
+        }
+
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Addition(10, 5));
+        }
+
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Addition(9, 3));
+        }
+    }
+}
