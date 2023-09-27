a8dd9d4770c1897aea086e9f72e1f14ab5f038ff
fivifri <emelianova.kp@phystech.edu>
2023-09-28 00:46:12 +0500

    Task6

---
+++ Tyuiu.EmelianovaKP.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.EmelianovaKP.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.EmelianovaKP.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60ef7ae9-27cb-404e-bfe3-4119e2317b4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.EmelianovaKP.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.EmelianovaKP.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.EmelianovaKP.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("The Array elements sum = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("The Array elements difference = " + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("The Array elements product = " + DataService.MultiplicationArray(arraynums));
+
+            Console.ReadKey();
+        }
+    }
+}
