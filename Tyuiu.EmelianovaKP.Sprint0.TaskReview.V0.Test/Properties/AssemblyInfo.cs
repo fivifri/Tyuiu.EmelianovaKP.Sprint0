0f18b517e1d4554c856511ae046b0c701a1b513f
fivifri <emelianova.kp@phystech.edu>
2023-09-28 13:55:37 +0500

    TaskReview

---
+++ Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.EmelianovaKP.Sprint0.TaskReview.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dfe8fe7e-a299-4973-a3bc-d4f0d0a45cb9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
